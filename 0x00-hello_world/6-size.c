d9b8a396058ef432a73dc5c6c86383dff57bba8d
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-10 19:37:08 -0800

    made and adjustment in task 6

--- 0x00-hello_world/6-size.c
+++ 0x00-hello_world/6-size.c
@@ -12,11 +12,11 @@ int main(void)
 	long long int lli;
 	float f;
 
-	printf("size of a char: %lu byte(s)\n", size of (c));
-	printf("size of an int %lu byte(s)\n", size of (i));
-	printf("size of a long int %lu byte(s)\n", size of (li));
-	printf("size of a long long int %lu byte(s)\n", size of (lli));
-	printf("size of a float %lu byte(s)\n", size of (f));
+	printf("size of a char: %lu byte(s)\n", sizeof(c));
+	printf("size of an int %lu byte(s)\n", sizeof(i));
+	printf("size of a long int %lu byte(s)\n", sizeof(li));
+	printf("size of a long long int %lu byte(s)\n", sizeof(lli));
+	printf("size of a float %lu byte(s)\n", sizeof(f));
 
 	return (0);
 }
