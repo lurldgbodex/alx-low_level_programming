fe7f49ac7e2cf34a772b8858554f1d05c4f0ebc1
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-10 18:38:31 -0800

    fix errror in task 5

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -1,13 +1,13 @@
 #include <stdio.h>
 
 /**
- * main - prints strings with proper grammer, but the outcome is a piece of art
- * 
- * retuns 0 
+ * main - prints strings with proper grammer,
+ * but the outcome is a piece of art,
+ * retuns 0 if exited proper, non-zero otherwise
  */
 int main(void)
 {
-	printf("with proper grammer, but the outcome is a piece of art"\n);
+	printf("with proper grammer, but the outcome is a piece of art,\n");
 
 	return 0;
 }
