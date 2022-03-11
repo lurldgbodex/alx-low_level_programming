c4cc32b5270059c0adfb2c7cd76a29aeb07ac969
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-10 20:29:26 -0800

    fix 101-quotes

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -5,7 +5,7 @@
  * main - prints exactly "and that piece of art is useful"
  * - Dora Korpar, 2015-10-19", 
  * followed by a new line, to the standard error.
- * Return: Alays 0, (success)
+ * Return: Always 0 (success)
  */
 int main(void)
 {
