66eb07b0d05b373b90e0af4728ab730cfc8e77ee
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-10 17:59:31 -0800

    fix error in task 4

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,12 @@
 #include <stdio.h>
-#include <string.h>
+
+/*
+ * main : uses puts to print to standard output
+ *
+ */
 
 int main(void)
 {
-	puts("\"programming is like building a multilingual puzzle");
+	puts("\"Programming is like building a multilingual puzzle");
 	return (0);
 }
