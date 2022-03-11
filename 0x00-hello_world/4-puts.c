95aec0288d1eb07ba9808aaf37988065ffa8fbf1
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-10 18:07:17 -0800

    Edit puts.c to comform to betty documentation

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,8 +1,8 @@
 #include <stdio.h>
 
 /*
- * main : uses puts to print to standard output
- *
+ * main - uses puts to print to standard output
+ * Return : 0 if exited properly, non-zero otherwise
  */
 
 int main(void)
