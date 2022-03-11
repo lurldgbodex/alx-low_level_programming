e2fe2a8613e7ed894deff87bb346cef605c9b7a8
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-10 17:40:01 -0800

    fix error in 4-puts

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -3,7 +3,7 @@
 
 int main(void)
 {
-	char str1 [] = "programming is like building a multilingual puzzle"
+	char str1 [] = "programming is like building a multilingual puzzle";
 	puts(\"st1);
 	return (0);
 }
