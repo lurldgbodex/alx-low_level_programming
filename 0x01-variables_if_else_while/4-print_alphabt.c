a76b9fd70209646d3287986a17022d6e41d77dfa
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-12 08:31:12 -0800

    added tasks 1 to 6

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - print the alphabet in lowercase,
+ * followed by a new line
+ * except q and e
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	char ch = 'a';
+
+	while (ch <= 'z')
+	{
+		if (ch != 'q' && ch != 'e')
+			putchar(ch);
+		ch++
+	}
+	putchar("\n")
+	return (0);
+}
---
+++ 0x01-variables_if_else_while/6-print_numberz.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+/**
+ * main - prints all single numbers of base 10
+ * starting from 0 using putchar
+ * followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+	int n;
+	
+	for (n = 0; n < 10; n++)
+	{
+		putchar(n);
+	}
+	putchar("\n");
+	return (0);
+}
