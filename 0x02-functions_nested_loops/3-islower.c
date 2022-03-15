24ba8514df1ca8726192692e2798348749e3e7d0
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-15 09:28:50 -0700

    apply betty documentation

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,18 +1,22 @@
 #include "main.h"
 
 /**
- * is_lower - checks or a lowercase character
- *
+ * _islower - checks or a lowercase character
+ * @c: input.
  * Return: 1 if c is lowercase and 0 if otherwise
  */
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 	{
+
 	return (1);
+
 	}
 	else
 	{
+
 	return (0);
+
 	}
 }	
