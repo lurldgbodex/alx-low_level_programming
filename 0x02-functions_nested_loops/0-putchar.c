fd3fdbc26f1ab742c48fe8f52524f42151142301
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-15 06:56:53 -0700

    null

--- 0x02-functions_nested_loops/0-putchar.c
+++ 0x02-functions_nested_loops/0-putchar.c
@@ -5,11 +5,11 @@
  */
 int main(void)
 {
-	char text[8] = "_putchar";
+	char c[8] = "_putchar";
 	int n;
 	for (n = 0; n < 8; n++)
 	{
-		_putchar(text[n]);
+		_putchar(c[n]);
 	}
 	_putchar('\n');
 
