ac7dd91082143c91fd18844751c5af7086bd6a74
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-15 08:57:04 -0700

    fix trailing whitespace in task 2

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -1,7 +1,9 @@
 #include "main.h"
-/** 
- * print_alphabet_x10 - prints 10 times the alphabet
- * in lowercase, followed by a new line
+
+/**
+ * print_alphabet_x10 - prints 10times the alphabet
+ * in lowercase
+ * followed by new line
  */
 void print_alphabet_x10(void)
 {
