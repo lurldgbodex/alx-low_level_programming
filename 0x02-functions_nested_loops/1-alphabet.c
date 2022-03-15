891aec0d200cc3ff07b70f41d67256429d300c75
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-15 08:09:31 -0700

    edit to betty style

--- 0x02-functions_nested_loops/1-alphabet.c
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -1,7 +1,6 @@
 #include "main.h"
 /**
- * main - prints the alphabet in lowercase, followed by an new line
- * Return: Always 0 (success)
+ * print_alphabet - prints the alphabet in lowercase, followed by an new line
  */
 void print_alphabet(void)
 {
@@ -13,6 +12,4 @@ void print_alphabet(void)
 		ch++;
 	}
 	_putchar('\n');
-
-	return;
 }
