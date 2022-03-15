a239126e8e1923db7dd78df1d223d34bde9789e0
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-15 09:17:53 -0700

    added task 3

---
+++ 0x02-functions_nested_loops/3-is_lower.c
@@ -0,0 +1,16 @@
+include "main.h"
+
+/**
+ * is_lower - checks or a lowercase character
+ *
+ * eturn: 1 if c is lowercase and 0 if otherwise
+ */
+int _islower(int c)
+{
+	if (c >= 'a' && c <= 'z')
+	{
+	return (1);
+	}
+	else
+	return (0);
+}	
