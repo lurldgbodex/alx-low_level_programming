6f9f66f46ffe0a00ff431ea9f48dd919a2b4e3e7
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-15 06:17:21 -0700

    added task 0

---
+++ 0x02-functions_nested_loops/0-puthcar.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * main - prints _putchar, followed by a new line
+ * Return: Always 0 (success)
+ */
+int main(void)
+{
+  putchar('_');
+  putchar('p');
+  putchar('u');
+  putchar('t');
+  putchar('c');
+  putchar('h');
+  putchar('a');
+  putchar('r');
+
+  return (0);
+}
