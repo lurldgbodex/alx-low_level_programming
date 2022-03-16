2ea8ee4646d1ae7fb851844aaa1d773ba727c4c6
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-16 06:23:29 -0700

    fix betty style coding

--- 0x02-functions_nested_loops/10-add.c
+++ 0x02-functions_nested_loops/10-add.c
@@ -1,6 +1,8 @@
 #include "main.h"
 /**
  * add - adds two integers and returns the result
+ * @a: parameter to be added to b
+ * @b: parameter to be added to a
  * Return: results of two added integers
  */
 int add(int a, int b)
