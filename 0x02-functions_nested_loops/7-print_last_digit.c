b66c0652eafe2cdd90fcbf23451c14b17bbdbbc9
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-16 05:15:58 -0700

    fix compile error in task 7

--- 0x02-functions_nested_loops/7-print_last_digit.c
+++ 0x02-functions_nested_loops/7-print_last_digit.c
@@ -1,14 +1,14 @@
 #include "main.h"
 /**
  * print_last_digit - prints the last digit of a number
- * @c: integer to check
+ * @r: integer to check
  * Return: the value of last integer
  */
-int print_last_digit(int c)
+int print_last_digit(int r)
 {
 	int a;
 
-	a = c % 10;
+	a = r % 10;
 
-	return (a)
+	return (a);
 }
