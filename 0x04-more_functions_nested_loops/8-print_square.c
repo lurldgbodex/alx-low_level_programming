3e710195b04905be44db69f55c2e2bf232f25001
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-17 10:06:09 -0700

    fix error

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -2,7 +2,7 @@
 
 /**
  * print_square - prints a square, followed by a new line
- * @siz: size of the square
+ * @size: size of the square
  */
 void print_size(int size)
 {
@@ -14,7 +14,7 @@ void print_size(int size)
 	{
 		int i, j;
 
-		for (i = 0, i < size; i++)
+		for (i = 0; i < size; i++)
 		{
 			for (j = 0; j < size; j++)
 			{
