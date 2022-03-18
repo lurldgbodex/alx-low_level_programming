a82e1d1912d6c8f6cf69f867b3243062f1b44c68
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-17 18:51:43 -0700

    fix compile error

--- 0x04-more_functions_nested_loops/9-fizz_buzz.c
+++ 0x04-more_functions_nested_loops/9-fizz_buzz.c
@@ -15,15 +15,15 @@ int main(void)
 
 	while (i <= 100)
 	{
-		if ( i % 3 = 0 && i % 5 = 0)
+		if ( i % 3 == 0 && i % 5 == 0)
 		{
 			printf("FizzBuzz");
 		}
-		else if (i % 3 = 0)
+		else if (i % 3 == 0)
 		{
 			printf("Fizz");
 		}
-		else if (i % 5 = 0)
+		else if (i % 5 == 0)
 		{
 			printf("Buzz");
 		}
