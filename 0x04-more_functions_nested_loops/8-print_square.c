3b879a5db594e5553585cddf32d23b66df540305
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-17 18:19:34 -0700

    fix error in task 8

--- 0x04-more_functions_nested_loops/8-print_square.c
+++ 0x04-more_functions_nested_loops/8-print_square.c
@@ -14,14 +14,14 @@ void print_size(int size)
 	{
 		int i, j;
 
-		for (i = 0; i < size; i++)
+		for (i = 1; i <= size; i++)
 		{
 			_putchar('#');
-			for (j = 0; j < size; j++)
+			for (j = 1; j < size; j++)
 			{
 				_putchar('#');
 			}
-			_putchar('\n');
+		_putchar('#');
 		}
 	}
 }
