6b7e53f3ae59ba346dc4a06c6456fd9213ff801e
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-24 09:05:57 -0700

    fix error

--- 0x06-pointers_arrays_strings/3-strcmp.c
+++ 0x06-pointers_arrays_strings/3-strcmp.c
@@ -4,33 +4,21 @@
  * _strcmp - compares two strings
  * @s1: string 1
  * @s2: string 2
- * 
+ *
  * Return: -1 is string less than the other, 1 if greater than the other and 0 if equal
  */
 int _strcmp(char *s1, char *s2)
 {
-	int n;
+  int counter, cmpval;
 
-	while (*s1 != '\0' && *s2 != '\0')
-	{
-		if (*s1 != *s2)
-		{
-			n = 1;
-		}
-		s1++;
-		s2++;
-	}
+  counter = 0;
 
-	if (n == 0)
-	{
-		return (0);
-	}
-	else if (n > 0)
-	{
-		return (1);
-	}
-	else
-	{
-		return (-1);
-	}
+  while (s1[counter] == s2[counter] && s1[counter] != '\0')
+    {
+      counter++;
+    }
+
+  cmpval = s1[counter] - s2[counter];
+
+  return (cmpval);
 }
