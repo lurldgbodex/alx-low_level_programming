511d1cad362fa12e3d7133fb034a63bbff5393ac
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-25 04:44:22 -0700

    added task 5

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,20 @@
+#include "main.h"
+
+/**
+ * string_toupper - changes all lowercase letters to uppercase
+ * @x: pointer to string
+ *
+ * Return: pointer to uppercase string
+ */
+char *string_toupper(char *x)
+{
+	int i = 0;
+
+	while (x[i] != '\0')
+	{
+		x[i] = x[i - 32];
+		i++;
+	}
+
+	return (x)
+}
