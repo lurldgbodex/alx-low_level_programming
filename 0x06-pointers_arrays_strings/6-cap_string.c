24245dc39275f5e46f1bbc0879d900cf1102083b
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-25 06:09:08 -0700

    added task 6

---
+++ 0x06-pointers_arrays_strings/6-cap_string.c
@@ -0,0 +1,37 @@
+#include "main.h"
+
+/**
+ * *cap_string - capitalize all worlds aof a string
+ * @s: string to modify
+ *
+ * Return: resulting string
+ */
+char *cap_string(char *s)
+{
+	int i, j;
+
+	char spec[13] = {' ', '\t', '\n', ',', ';', '.', '!', '?', '"', '(', ')', '{', '}'};
+
+	i = 0;
+
+	while (s[i] != '\0')
+	{
+		if (i == 0 && s[i] >= 'a' && s[i] <= 'z')
+		{
+			s[i] -= 32;
+		}
+		for (j = 0; j < 13; j++)
+		{
+			if (s[i] == spec[j])
+			{
+				if (s[i + 1] >= 'a' && s[i + 1] <= 'z')
+				{
+					s[i + 1] -= 32;
+				}
+			}
+		}
+		i++;
+	}
+
+	return (s);
+}
