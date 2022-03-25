9f406171a984552b41c3a5a9d14b00fcec788e5c
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-25 05:18:33 -0700

    fix error

--- 0x06-pointers_arrays_strings/5-string_toupper.c
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -1,7 +1,7 @@
 #include "main.h"
 
 /**
- * string_toupper - changes all lowercase letters to uppercase
+ * *string_toupper - changes all lowercase letters to uppercase
  * @x: pointer to string
  *
  * Return: pointer to uppercase string
@@ -14,7 +14,7 @@ char *string_toupper(char *x)
 	{
 		if (x[i] >= 97 && x[i] <= 122)
 		{
-			x[i] = x[i] - 32
+			x[i] = x[i] - 32;
 		}
 		i++;
 	}
