fa811904252db9ce157a629fabe5bae26ddea84e
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-29 21:00:25 -0700

    add task 4

---
+++ 0x07-pointers_arrays_strings/4-strpbrk.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * *_strpbrk - searches a string or any of a set bytes
+ * @s: string to check
+ * @accept: string to check against
+ *
+ * Return: pointer to s that matches or null if ther isn't
+ */
+char *_strpbrk(char *s, char *accept)
+{
+	char *p;
+	int i, j;
+
+	p = &s;
+
+	for (i = 0; s[i] !='\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (accept[j] == s[i])
+			{
+				p = s[i];
+			}
+		}
+	}
+	return (p);
+}
