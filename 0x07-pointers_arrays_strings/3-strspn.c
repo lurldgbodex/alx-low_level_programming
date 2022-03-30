b7666f49d493a8030421fa8b61b77c68c44dff0f
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-29 20:15:59 -0700

    add task 3

---
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -0,0 +1,28 @@
+#include "main.h"
+
+/**
+ * _stspn - gets the length of a prefix substring
+ * @s: string to evaluate
+ * @accept: string containing the list of characters to match s
+ *
+ * Return: number of bytes in s contained in accept
+ */
+unsigned int _stspn(char *s, char *accept)
+{
+	unsigned int i, j, count;
+	count = 0;
+
+	for (i = 0; s[i] != '\0'; i++)
+	{
+		for (j = 0; accept[j] != '\0'; j++)
+		{
+			if (s[i] == accept [j])
+			{
+				count ++;
+			}
+			else
+				break;
+		}
+	}
+	return (count);
+}
