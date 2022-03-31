7e40528d7ef3bd21e6aae4eb4ddad716b62d047f
Lurldgbodex <professionaleditor6@gmail.com>
2022-03-31 02:15:40 -0700

    fix betty

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -17,9 +17,9 @@ unsigned int _strspn(char *s, char *accept)
 	{
 		for (j = 0; accept[j] != '\0'; j++)
 		{
-			if (s[i] == accept [j])
+			if (s[i] == accept[j])
 			{
-				count ++;
+				count++;
 				break;
 			}
 		}
