264759ed94478cf6b979efeb937b550bea9461e4
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-01 13:32:27 -0700

    add task 0

---
+++ 0x0A-argc_argv/0-whatsmyname.c
@@ -0,0 +1,13 @@
+#include  <stdio.h>
+
+/**
+ * main - prints the name of the program followed by a new line
+ * @argc: counts the number of arguments passed into command line
+ * @argv: arrays that holds the string value of the command entered
+ */
+int main(int argc, char* argv[])
+{
+	(void)argc;
+
+	printf("%s\n", argv[0]);
+}
