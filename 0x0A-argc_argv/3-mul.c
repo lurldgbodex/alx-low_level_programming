16271e4d6000a8abfb9125d9661eb41756d04565
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-01 18:06:24 -0700

    add task 3

---
+++ 0x0A-argc_argv/3-mul.c
@@ -0,0 +1,32 @@
+#include <stdlib.h>
+#include <stdio.h>
+
+/**
+ * main - add two postive numbers
+ * @argc: number of command line arguments
+ * @argv: array of arguments
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, mul;
+
+	mul = 0;
+
+	if (argc < 2)
+	{
+		printf("Error\n");
+		return (1);
+	}
+	else
+	{
+		for (i = 1; i < argc; i++)
+		{
+			mul *= atoi(argv[i]);
+		}
+	}
+	printf("%d\n", mul);
+
+	return (0);
+}
