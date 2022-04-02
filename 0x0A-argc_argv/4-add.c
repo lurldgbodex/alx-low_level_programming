2f1758d85c7ed46c159516b46ea1a63d549ea1e1
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-01 18:55:35 -0700

    add task 4

---
+++ 0x0A-argc_argv/4-add.c
@@ -0,0 +1,41 @@
+#include <stdio.h>
+#include <stdlib.h>
+
+/**
+ * main - adds positive numbers
+ * @argc: number of command line arguments
+ * @argv: array of command line string
+ *
+ * Return: always 0
+ */
+int main(int argc, char *argv[])
+{
+	int i, result;
+
+	result = 0;
+
+	if (argc < 2)
+	{
+		printf("0\n");
+		return (0);
+	}
+
+	if (argc > 1)
+	{
+		for (i = 0; i < argc; i++)
+		{
+			if (argv[i] < '0' || argv[i] > '9')
+			{
+				printf("Error\n");
+				return (1);
+			}
+			else
+			{
+				result += atoi(argv[i]);
+			}
+		}
+	}
+	printf("%d\n", result);
+
+	return (0);
+}
