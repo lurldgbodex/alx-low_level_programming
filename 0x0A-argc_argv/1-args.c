c78c376c62ba60ff235cb033e57895c017ef0e80
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-01 17:19:03 -0700

    fix erro

--- 0x0A-argc_argv/1-args.c
+++ 0x0A-argc_argv/1-args.c
@@ -4,12 +4,14 @@
  * main - prints the number of arguments passed into it
  * @argc: number of arguments passed into it
  * @argv: aray of argumensts
+ *
+ * Return: always 0
  */
 int main(int argc, char *argv[])
 {
 	(void)argv;
 
-	printf("%d\n", argc);
+	printf("%d\n", argc - 1);
 
 	return (0);
 }
