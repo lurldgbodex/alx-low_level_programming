64ac7ac11f27bfd8795349dd110befddb2912bf3
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-05 17:41:54 -0700

    fix error

--- 0x0B-malloc_free/2-str_concat.c
+++ 0x0B-malloc_free/2-str_concat.c
@@ -12,7 +12,7 @@ char *str_concat(char *s1, char *s2)
 {
 	char *p;
 	unsigned int i, size, len1, len2, j;
-
+	
 	if (s1 == NULL)
 	{
 		s1 = "";
@@ -21,45 +21,43 @@ char *str_concat(char *s1, char *s2)
 	{
 		s2 = "";
 	}
-
+	
 	len1 = 0;
-
 	while (s1[len1] != '\0')
 	{
 		len1++;
 	}
-
+	
 	len2 = 0;
-
 	while (s2[len2])
 	{
 		len2++;
 	}
-
+	
 	size = len1 + len2;
 	p = malloc(sizeof(char) * (size + 1));
-
+	
 	if (p == NULL)
 	{
 		return (NULL);
 	}
-
+	
 	i = 0;
-
-	while (i <= size)
+	
+	while (i < len1)
 	{
 		p[i] = s1[i];
 		i++;
 	}
-
+	
 	j = 0;
-
+	
 	while (i <= size)
 	{
 		p[i] = s2[j];
 		i++;
 		j++;
 	}
-
+	
 	return (p);
 }
