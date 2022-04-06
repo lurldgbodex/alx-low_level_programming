1101b2a1efbbdae41320320ffd6f98ea35c4eb5e
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-06 16:50:54 -0700

    refactor code in advance task 2

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -2,42 +2,81 @@
 #include "main.h"
 
 /**
- * strtow - splits a string into two
- * @str: pointer to string
+ * count_word - counts the number of words in a given string
+ * @str: string to evaluate
  *
- * Return: pointer to te array of string
- * NULL if str = NULL or str = ""
+ * Return: no of words
+ */
+int count_word(char *str)
+{
+	int count, i, j;
+
+	count = 0;
+	i = 0;
+
+	for (j = 0; str[j] != '\0'; j++)
+	{
+		if (str[j] == ' ')
+			i = 0;
+		else if (i == 0)
+		{
+			i = 1;
+			count++;
+		}
+	}
+
+	return (count);
+}
+
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ *
+ * Return: pointer to an array of string 
+ * or NULL if failure
  */
 char **strtow(char *str)
 {
-	char **result;
-	int len, i;
-	char delimeter[] = " ";
+	char **result, *tmp;
+	int i, j, k, len, words, start, end;
 
-	if (str == NULL || *str == *delimeter)
-	{
-		return (NULL);
-	}
 	len = 0;
+	j = 0;
+	k = 0;
 
 	while (str[len])
 		len++;
-	result = malloc(sizeof(char *) * (len + 1));
+	words = count_word(str);
+
+	if (words == 0)
+		return (NULL);
+	result = (char **)malloc(sizeof(char *) * (words + 1));
 
 	if (result == NULL)
 		return (NULL);
-	for (i = 0; i < len; i++)
+
+	for (i = 0; i <= len; i++)
 	{
-		if (str[i] == *delimeter || str[i] == '\0')
+		if (str[i] == ' ' || str[i] == '\0')
 		{
-			result[i] = '\0';
+			if (j)
+			{
+				end = i;
+				tmp = (char *)malloc(sizeof(char) * (j + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				result[k] = tmp - j;
+				k++;
+				j = 0;
+			}
 		}
-		else
-		{
-			*(result[i]) = str[i];
-		}
+		else if (j++ == 0)
+			start = i;
 	}
-	result[i] = '\0';
+	result[k] == NULL;
 
 	return (result);
 }
