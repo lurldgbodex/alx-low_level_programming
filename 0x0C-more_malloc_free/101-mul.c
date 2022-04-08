6e2dec83d55f90498970cf8d44457681ecbf6f0b
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-07 19:21:14 -0700

    fix error

--- 0x0C-more_malloc_free/101-mul.c
+++ 0x0C-more_malloc_free/101-mul.c
@@ -39,6 +39,15 @@ int _strlen(char *s)
 }
 
 /**
+ * errors - handles errors for main
+ */
+void errors(void)
+{
+	printf("Error\n");
+	exit(98);
+}
+
+/**
  * main - multiplies two positive numbers
  * @argc: number of arguments
  * @argv: array of arguments
@@ -48,18 +57,15 @@ int _strlen(char *s)
 int main(int argc, char *argv[])
 {
 	char *s1, *s2;
-	int len, len1, len2, i, digit, digit1, *result, carry, a;
+	int len, len1, len2, i, digit1, digit2, *result, carry, a;
 
 	s1 = argv[1], s2 = argv[2];
 	if (argc != 3 || !is_digit(s1) || !is_digit(s2))
-	{
-		printf("Error\n");
-		exit(98);
-	}
+		errors()
 	len1 = _strlen(s1);
 	len2 = _strlen(s2);
 	len = len1 + len2 + 1;
-	result = malloc(sizeof(*result) * len);
+	result = malloc(sizeof(int) * len);
 
 	if (!result)
 	{
@@ -75,8 +81,8 @@ int main(int argc, char *argv[])
 		carry = 0;
 		for (len2 = _strlen(s2) - 1; len2 >= 0; len2--)
 		{
-			digit = s2[len2] - '0';
-			carry += result[len1 + len2 + 1] + (digit1 * digit);
+			digit2 = s2[len2] - '0';
+			carry += result[len1 + len2 + 1] + (digit1 * digit2);
 			result[len1 +len2 + 1] = carry % 10;
 			carry /= 10;
 		}
