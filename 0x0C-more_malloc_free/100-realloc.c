e17352b3cb8846cb2154bcfd28fcbdbfd6822e61
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-07 18:29:59 -0700

    fix betty

--- 0x0C-more_malloc_free/100-realloc.c
+++ 0x0C-more_malloc_free/100-realloc.c
@@ -17,6 +17,7 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 
 	if (new_size == old_size)
 		return (ptr);
+
 	if (new_size == 0 && ptr)
 	{
 		free(ptr);
@@ -24,7 +25,9 @@ void *_realloc(void *ptr, unsigned int old_size, unsigned int new_size)
 	}
 	if (!ptr)
 		return (malloc(new_size));
+
 	pt = malloc(new_size);
+	
 	if(!pt)
 	{
 		free(pt);
