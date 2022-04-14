92dde75052c82b2a19ed3fa9c9a7e547af980704
Lurldgbodex <professionaleditor6@gmail.com>
2022-04-14 09:55:13 -0700

    initial commit with task 0 through 3

---
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -0,0 +1,26 @@
+#include <stdarg.h>
+
+/**
+ * sum_them_all - calculates the sum of all its parameters
+ * @n: number of arguments passed to the function
+ *
+ * Return: resulting sum
+ */
+int sum_them_all(const unsigned int n, ...)
+{
+	unsigned int i;
+	int sum = 0;
+	va_list arglist;
+
+	if (n == 0)
+		return (0);
+
+	va_start(arglist, n);
+
+	for (i = 0; i < n; i++)
+		sum += va_arg(arglist, int);
+
+	va_end(arglist);
+	
+	return (sum);
+}
---
+++ 0x10-variadic_functions/2-print_strings.c
@@ -0,0 +1,33 @@
+#include "variadic_functions.h"
+
+/**
+ * print_strings - prints strings
+ * @separator: separator between strings
+ * @n: number of arguments
+ */
+void print_strings(const char *separator, const unsigned int n, ...)
+{
+	unsigned int i;
+	char *s;
+	va_list strings;
+
+	if (separator == NULL)
+		separator = "";
+
+	va_start(strings, n);
+
+	for (i = 0; i < n; i++)
+	{
+		s = va_arg(strings, char *);
+
+		if (s == NULL)
+			s = "(nil)";
+		printf("%s", s);
+		if (i < n - 1)
+		{
+			printf("%s", separator);
+		}
+	}
+	printf("\n");
+	va_end(strings);
+}
