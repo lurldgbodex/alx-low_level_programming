26aeb538d4594085e6210529c7a7a99b6bf48d5c
Lurldgbodex <professionaleditor6@gmail.com>
2022-06-10 16:56:19 -0700

    update file

--- 0x17-doubly_linked_lists/2-add_dnodeint.c
+++ 0x17-doubly_linked_lists/2-add_dnodeint.c
@@ -18,7 +18,7 @@ dlistint_t *add_dnodeint(dlistint_t **head, const int n)
 		return (NULL);
 
 	new_node->n = n;
-	*head->prev = new_node;
+	(*head)->prev = new_node;
 	new_node->next = *head;
 	*head = new_node;
 	return (new_node);
