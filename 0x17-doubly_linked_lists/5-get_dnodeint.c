ac91a597cf10dca0f780f6ad80196fe2b0d61d1b
lurldgbodex <42384410+lurldgbodex@users.noreply.github.com>
2022-06-11 01:47:24 +0100

    Update 5-get_dnodeint.c

--- 0x17-doubly_linked_lists/5-get_dnodeint.c
+++ 0x17-doubly_linked_lists/5-get_dnodeint.c
@@ -1,11 +1,11 @@
 #include "lists.h"
 
 /**
- * get_dnodeint_at_index - returns the nth node of a dlistint_t list
- * @head: pointer to list
- * @index: index of node to return
+ * get_dnodeint_at_index - returns the nth node of a dlistint_t linked list
+ * @head: a pointer to the starting node
+ * @index: the index of the node to fetch
  *
- * Return: address of the node and NUll if node does not exist
+ * Return: the address of the fetched node
  */
 dlistint_t *get_dnodeint_at_index(dlistint_t *head, unsigned int index)
 {
