149444a2a47fbbda4bac476eacab356aefe4666b
Lurldgbodex <professionaleditor6@gmail.com>
2022-07-03 05:08:30 -0700

    update file

--- 0x1A-hash_tables/6-hash_table_delete.c
+++ 0x1A-hash_tables/6-hash_table_delete.c
@@ -1,36 +1,30 @@
 #include "hash_tables.h"
 
 /**
- * free_list - frees a linked list
- * @head: hash_node_t list to be freed
- */
-void free_list(hash_node_t *head)
-{
-	hash_node_t *temp;
-
-	while (head)
-	{
-		temp = head->next;
-		free(head->key);
-		free(head->value);
-		free(head);
-		head = temp;
-	}
-}
-
-/**
- * hash_table_delete - deletes a hash table
- * @ht: hash table to be deleted
- */
+  * hash_table_delete - deletes a hash table.
+  * @ht: hash table to be deleted.
+  *
+  */
 void hash_table_delete(hash_table_t *ht)
 {
-	unsigned long int i;
+	hash_node_t *temp, *temp2;
+	unsigned long int i = 0;
 
-	if (!ht)
+	if (ht == NULL)
 		return;
 
 	for (i = 0; i < ht->size; i++)
-		free_list(ht->array[i]);
+	{
+		temp = ht->array[i];
+		while (temp != NULL)
+		{
+			temp2 = temp->next;
+			free(temp->key);
+			free(temp->value);
+			free(temp);
+			temp = temp2;
+		}
+	}
 	free(ht->array);
 	free(ht);
 }
